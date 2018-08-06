8906a6422c427b8976fb20ddf1dd39ed80e490b7
MandyArthur <Daviddcg8@kent.ac.uk>
2018-08-06 16:37:18 +0100

    initial commit

---
+++ Hour2/hour2.js
@@ -0,0 +1,11 @@
+
+
+var cTemp = 20; // temperature in Celsius
+// Let's be generous with brackets
+var hTemp = ((cTemp*9)/5) + 32;
+
+document.write("Celsius: " + cTemp +" degrees<br/>"); 
+
+document.write("Fahrenheit: "+ hTemp + " degrees"); 
+
+
